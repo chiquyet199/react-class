d8cb4f205289010f83d43616256f05703b6d24a2
Fram <fram@Frams-MacBook-Pro.local>
2019-07-22 15:17:10 +0700

    Normal component rerender

--- testing-react-features/src/App.js
+++ testing-react-features/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let counter = 1
+  const [count, setCount] = useState(0)
+  const onBtnClick = () => {
+    counter = count + 1
+    setCount(counter)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Child/>
+        <h1>Count: {count}</h1>        
+        <h2>This is static content</h2>
+        <button onClick={onBtnClick}>Set Count To 1</button>
       </header>
     </div>
   );
+}
+
+const Child = function(){
+  console.log('child component re-render')
+  return <h1>Hello</h1>
 }
 
 export default App;
